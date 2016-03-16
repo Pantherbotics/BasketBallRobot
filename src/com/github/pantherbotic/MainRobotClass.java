cccd2ef42d9c50e21e0aa81aba9e203aa8acfefc
d4rkfly3r <npjoshf@gmail.com>
2016-03-16 11:35:16 -0700

    30 Lines... Working code (may need to flip some signs)

--- src/com/github/pantherbotic/MainRobotClass.java
+++ src/com/github/pantherbotic/MainRobotClass.java
@@ -1,40 +1,30 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 package com.github.pantherbotic;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.Victor;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class MainRobotClass extends IterativeRobot {
 
-    public final Jaguar motoBackLeft = new Jaguar(1, 1);
-    public final Jaguar motoFrontLeft = new Jaguar(1, 2);
-    public final Jaguar motoFrontRight = new Jaguar(1, 3);
-    public final Jaguar motoBackRight = new Jaguar(1, 4);
+    public final Jaguar motoBackLeft = new Jaguar(1, 1), motoFrontLeft = new Jaguar(1, 2), motoFrontRight = new Jaguar(1, 3), motoBackRight = new Jaguar(1, 4), motoShoot1 = new Jaguar(1, 5), motoShoot2 = new Jaguar(1, 6);
+    public final Victor motoPickUp = new Victor(1, 7), motoBelt = new Victor(1, 8);
     public final Joystick rightJoystick = new Joystick(1);
 
     public void teleopPeriodic() {
-        double xAxis = rightJoystick.getX(),
-                yAxis = rightJoystick.getY(),
-                twist = rightJoystick.getTwist(),
-                throttle = rightJoystick.getThrottle();
+        double xAxis = rightJoystick.getX(), yAxis = rightJoystick.getY(), twist = rightJoystick.getTwist(), shooterSpeed = (rightJoystick.getThrottle() + 1) / 2;
+
+        boolean trigger = rightJoystick.getTrigger();
 
         this.motoBackLeft.set(xAxis - yAxis + twist);
         this.motoFrontLeft.set(-xAxis - yAxis + twist);
         this.motoFrontRight.set(-xAxis + yAxis + twist);
         this.motoBackRight.set(-xAxis + yAxis + twist);
+
+        this.motoBelt.set(trigger ? 1 : 0);
+        this.motoPickUp.set(rightJoystick.getRawButton(7) ? 1 : 0);
+
+        this.motoShoot1.set(shooterSpeed);
+        this.motoShoot2.set(-shooterSpeed);
     }
 }
